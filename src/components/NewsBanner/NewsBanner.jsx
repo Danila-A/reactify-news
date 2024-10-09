ce7e9c2e3e9f9cc28c45ef77812f7738b14257f2
Danila-A <dan4ik.rus@mail.ru>
2024-10-09 23:46:05 +0300

    Initial commit

---
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.css';
+import { formatTimeAgo } from '../../helpers/formatTimeAgo';
+import Image from '../Image/Image';
+
+export default function NewsBanner({ item }) {
+    return (
+        <section className={ styles.banner }>
+
+            <Image image={item?.image}/>
+            <h3 className={ styles.titel }>{ item.title }</h3>
+            <p className={ styles.extra }>
+                { formatTimeAgo(item.published) } by { item.author }
+            </p>
+
+        </section>
+    )
+}
